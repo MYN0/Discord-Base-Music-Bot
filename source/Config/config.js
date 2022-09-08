0fc2f8b3036958d727dc74ccec46f3aad81a361f
MYN0ᴰᵉᵛ <91677949+MYN0@users.noreply.github.com>
2022-09-08 21:38:09 +0530

    Update config.js

--- source/Config/config.js
+++ source/Config/config.js
@@ -1,11 +1,11 @@
 export default {
   errorColor: "#cc1616",
-  errorEmoji: "<:error_emoji:1016717148611170355>",
+  errorEmoji: "ERROR EMOJI",
   mainColor: "#00d9ff",
   token:
-    "MTAxNDc5Mjc0MjUyNzgyODA1OA.GIrRAP.jMOkx4VRAvopZmQI8gJzmfbsRwo9lnKQ-QzxFI", //This is mandatory
+    "YOUR BOT TOKEN", //This is mandatory
   mongooseConnectionURI: "",
   prefix: ".",
   successEmbedColor: "#12cc15",
-  successEmoji: "<:Cheeze_Yes:1013377205813911594>",
+  successEmoji: "SUCCESS_EMOJI",
 };
